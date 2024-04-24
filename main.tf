3e82e2cb48136f88d489e2119eea8adcee886523
Gopakumar <132288346+Devopslearner2023@users.noreply.github.com>
2024-04-24 22:08:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,99 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
+provider "aws" {
+  profile = "default"
+  region  = "ap-south-1"
+}
+
+
+resource "aws_vpc" "myvpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet_1a" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "subnet_1a"
+  }
+}
+
+resource "aws_internet_gateway" "mygw" {
+  vpc_id = aws_vpc.myvpc.id
+
+  tags = {
+    Name = "mygw"
+  }
+}
+
+resource "aws_route_table" "rt_public" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.mygw.id
+  }
+
+  tags = {
+    Name = "rt_public"
+  }
+}
+
+
+resource "aws_route_table_association" "associate-1a-rt" {
+  subnet_id      = aws_subnet.subnet_1a.id
+  route_table_id = aws_route_table.rt_public.id
+}
+
+resource "aws_instance" "web001" {
+  ami           = "ami-007020fd9c84e18c7"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.subnet_1a.id
+  vpc_security_group_ids = [ aws_security_group.webservers.id ]
+  key_name = "Aws-key-2024"
+  user_data = file("script.sh")
+  tags = {
+    Name = "Web001"
+  }
+}
+
+resource "aws_security_group" "webservers" {
+  name        = "webservers-8080"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = aws_vpc.myvpc.id
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Name = "allow_tls"
+  }
+}
