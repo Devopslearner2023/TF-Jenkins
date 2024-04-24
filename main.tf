1259da6c9eec150f8381b7cb767e79c26a3cb5fe
Gopakumar <132288346+Devopslearner2023@users.noreply.github.com>
2024-04-24 22:09:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  profile = "default"
-  region  = "ap-south-1"
-}
-
 
 resource "aws_vpc" "myvpc" {
   cidr_block = "10.0.0.0/16"
